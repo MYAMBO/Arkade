9a5ffa5f040fca9651779202c379a45a6d8bd2ba
MartinB-E <martin.bonte@epitech.eu>
2025-03-26 11:50:33 +0100

    fix(main.cpp): fix main by working with games

--- Core/src/main.cpp
+++ Core/src/main.cpp
@@ -7,62 +7,30 @@
 
 #include <dlfcn.h>
 
-#include "test.hpp"
 #include "Core.hpp"
-#include "Nibbler.hpp"
-#include "IDisplayModule.hpp"
-
-IDisplayModule *changeInstance1(void *before)
-{
-    dlclose(before);
-    void* handle = dlopen("lib/arcade_ncurses.so", RTLD_LAZY);
-
-    if (!handle) {
-        std::cerr << "Erreur de chargement: " << dlerror() << std::endl;
-        return NULL;
-    }
-
-    CreateInstanceFunc createInstance = (CreateInstanceFunc)dlsym(handle, "createInstance");
-
-    return createInstance();
-}
-
-IDisplayModule *changeInstance2(void *before)
-{
-    dlclose(before);
-    void* handle = dlopen("lib/arcade_sfml.so", RTLD_LAZY);
-
-    if (!handle) {
-        std::cerr << "Erreur de chargement: " << dlerror() << std::endl;
-        return NULL;
-    }
-
-    CreateInstanceFunc createInstance = (CreateInstanceFunc)dlsym(handle, "createInstance");
-
-    return createInstance();
-}
 
 int main()
 {
-    std::string i = NCURSES;
+    std::string i = SFML;
     int input = -1;
 
     Core core;
 
-    Nibbler nibbler;
-    auto &objects = nibbler.getObjects();
-    instance->initObject(objects);
-    instance->openWindow();
+    auto displayModuleList = core.getDisplayModuleList();
+    auto gameModuleList = core.getGameModuleList();
+    auto game = gameModuleList[NIBBLER];
+    auto &objects = game->getObjects();
+    displayModuleList[SFML]->initObject(objects);
+    displayModuleList[SFML]->openWindow();
     while (input != 'p') {
-        input = instance->getInput();
-        nibbler.update(instance->getMousePos(), IGameModule::click::NOTHING, input);
-        instance->display(objects);
-        if (input == 'c') {
-            instance->closeWindow();
-            destroyInstance(instance);
-            instance = changeInstance1(handle);
-            instance->initObject(objects);
-            instance->openWindow();
+        input = displayModuleList[i]->getInput();
+        game->update(displayModuleList[i]->getMousePos(), input);
+        displayModuleList[i]->display(objects);
+        if (input == 'c' && i == SFML) {
+            displayModuleList[i]->closeWindow();
+            i = NCURSES;
+            displayModuleList[i]->initObject(objects);
+            displayModuleList[i]->openWindow();
         }
         if (input == 'v' && i == NCURSES) {
             displayModuleList[i]->closeWindow();
