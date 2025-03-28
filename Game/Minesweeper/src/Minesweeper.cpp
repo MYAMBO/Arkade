28c4deda6d36ec59400d25fca9bd2191d4690d7c
MartinB-E <martin.bonte@epitech.eu>
2025-03-28 15:39:24 +0100

    fix(Minesweeper/): add member function

--- Game/Minesweeper/src/Minesweeper.cpp
+++ Game/Minesweeper/src/Minesweeper.cpp
@@ -8,11 +8,43 @@
 #include "Minesweeper.hpp"
 
 Minesweeper::Minesweeper()
+    : _objects(*new std::map<std::string, std::unique_ptr<IObject>>())
 {
-    
 }
 
 Minesweeper::~Minesweeper()
 {
+}
 
+std::string Minesweeper::getName() const
+{
+    return MINESWEEPER;
+}
+
+std::size_t Minesweeper::getScore() const
+{
+    return 0;
+}
+
+std::map<std::string, std::unique_ptr<IObject>>& Minesweeper::getObjects()
+{
+    return _objects;
+}
+
+bool Minesweeper::update(std::pair<int, int> mousePos, int input)
+{
+    (void)mousePos;
+    (void)input;
+    return false;
+}
+
+void Minesweeper::deleteObject(std::string name)
+{
+    _objects.erase(name);
+}
+
+void Minesweeper::addObject(std::string type, std::string name)
+{
+    (void)type;
+    (void)name;
 }
