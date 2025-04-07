79d228488ba9d3e1681eddf26f210b39f056b396
MartinB-E <martin.bonte@epitech.eu>
2025-04-07 09:44:01 +0200

    fix(test.cpp): fix crash when launch tests

--- tests/testCore.cpp
+++ tests/testCore.cpp
@@ -1,32 +1,22 @@
-/*
-** EPITECH PROJECT, 2025
-** Arkade
-** File description:
-** testCore.cpp
-*/
-
 #include <iostream>
 #include <criterion/redirect.h>
 #include <criterion/criterion.h>
-
 #include "Core.hpp"
 
-Test(Core, getGrahicLib, .init=cr_redirect_stdout)
+Test(Core, getDisplayModuleList, .init=cr_redirect_stdout)
 {
     Core core;
-
     for (auto elt : core.getDisplayModuleList())
         std::cout << elt.first << "\n";
-    std::cout << std::fflush(stdout);
+    std::cout << std::flush;
     cr_assert_stdout_eq_str("NCURSES\nSFML\n");
 }
 
-Test(Core, getGameLib, .init=cr_redirect_stdout)
+Test(Core, getGameModuleList, .init=cr_redirect_stdout)
 {
     Core core;
-
     for (auto elt : core.getGameModuleList())
         std::cout << elt.first << "\n";
-    std::cout << std::fflush(stdout);
+    std::cout << std::flush;
     cr_assert_stdout_eq_str("MINESWEEPER\nNIBBLER\n");
 }
