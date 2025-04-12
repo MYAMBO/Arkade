68dd7d55d5799f338915465e4c0d8ac14d6b634b
Antoine Orange <antoine.orange@epitech.eu>
2025-04-12 19:00:39 +0200

    fix(SDL2): adding custom destructor on window and renderer smart pointers

--- Graphic/SDL2/include/SDL2.hpp
+++ Graphic/SDL2/include/SDL2.hpp
@@ -32,8 +32,9 @@ public:
 
     void display(std::map<std::string, std::unique_ptr<Arcade::IObject>>&);
 private:
-    std::unique_ptr<SDL_Window, void (*)(SDL_Window*)> _window;
-    std::unique_ptr<SDL_Renderer, void (*)(SDL_Renderer*)> _renderer;
+    std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> _window{nullptr, SDL_DestroyWindow};
+    std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> _renderer{nullptr, SDL_DestroyRenderer};
+    std::map<std::string, bool> _isLoad;
     SDL_Event _event;
 };
 
